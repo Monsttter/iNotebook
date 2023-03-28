978f4a9a2b1ba56a50e4062eb6b50a59059526f7
Rahul Choudhary <rahul.choudhary.eee20@itbhu.ac.in>
2023-03-28 21:53:49 +0530

    Video 45 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,14 @@
 const express= require("express");
+const User= require("../models/User");
 
 const router= express.Router();
 
-router.get("/", (req,res)=> {
-    const obj= {
-        a: "rahul",
-        num: 20
-    }
-    res.json(obj);
+// Create a User using : POST "/api/auth/". Doesn't require auth.
+router.post("/", (req,res)=> {
+    console.log(req.body);
+    const user= new User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports= router;
